6db985922350b23e4cad45da3b17d484b81d79de
wwpp1ppww <wwpp1ppww@gmail.xom>
2023-03-08 21:02:22 +0300

    init 4

--- src/main/java/com/example/demo/dao/PersonDAO.java
+++ src/main/java/com/example/demo/dao/PersonDAO.java
@@ -3,21 +3,61 @@ package com.example.demo.dao;
 import com.example.demo.models.Person;
 import org.springframework.stereotype.Component;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonDAO {
-    private List<Person> people;
+    //private List<Person> people;
+    private static final String URL = "jdbc:postgresql://localhost:5432/Demo";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "1";
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
 
     {
-        people = new ArrayList<>();
-
-        people.add(new Person(1, "Tom"));
-        people.add(new Person(2, "Jack"));
+//        people = new ArrayList<>();
+//        people.add(new Person(1, "Tom"));
+//        people.add(new Person(2, "Jack"));
     }
 
     public List<Person> index(){
+        List<Person> people = new ArrayList<>();
+
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM Person";
+            ResultSet resultSet = statement.executeQuery(SQL);
+
+            while(resultSet.next()) {
+                Person person = new Person();
+
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("name"));
+
+
+                people.add(person);
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
         return people;
     }
 }
